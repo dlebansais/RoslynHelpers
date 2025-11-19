525a1e97a297478bd64712ca80e1bddf52130f73
David Le Bansais <dlebansais@gmail.com>
2025-11-19 21:40:23 +0100

    CI fix.

--- RoslynHelpers/UsingDirectiveHelper.cs
+++ RoslynHelpers/UsingDirectiveHelper.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Contracts;
-using Microsoft.CodeAnalysis;
 
 /// <summary>
 /// Helper to perform operations on using directives.
