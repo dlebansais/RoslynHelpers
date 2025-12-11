2c6825b25ee166f7251f94d24885339af28857d7
David Le Bansais <dlebansais@gmail.com>
2025-12-11 18:44:52 +0100

    Typo fix.

--- Test/RoslynHelpers.Test/AssemblyInfo.cs
+++ Test/RoslynHelpers.Test/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-#if DO_NOT_PARALELIZE_TEST_FOR_STRYKER
+#if DO_NOT_PARALLELIZE_TEST_FOR_STRYKER
 [assembly: DoNotParallelize]
 #else
 [assembly: Parallelize(Scope = ExecutionScope.MethodLevel)]
